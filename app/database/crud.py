56564ddfa533d657826ae48072d3adff49dcd799
rovaldavc <arshia.talimian86@gmail.com>
2026-06-30 11:36:13 +0330

    Added Login/Logout core function into crud file

--- app/database/crud.py
+++ app/database/crud.py
@@ -1,10 +1,11 @@
-from database.models import User
-from fastapi import HTTPException, Depends
+from database.models import User, UserSession
+from fastapi import HTTPException, Depends, Response, Request
+from fastapi.security import OAuth2PasswordRequestForm
 from database.schemas import UserCreate
 from sqlalchemy.orm import Session
 from database.database import get_db
-from security.hash_password import get_password_hash
-
+from security.hash_password import get_password_hash, verify_password
+from security.authentication import create_session, COOKIE_NAME
 
 def crud_get_user(user_id:int, db:Session):
     user = db.query(User).filter(User.id == user_id).first()
@@ -12,7 +13,7 @@ def crud_get_user(user_id:int, db:Session):
         raise HTTPException(status_code=404, detail="User not found.")
     return user
 
-def crud_post_user(user:UserCreate, db:Session):
+def crud_sign_up(user:UserCreate, db:Session):
     if db.query(User).filter(User.username == user.username).first():
         raise HTTPException(status_code=422, detail="User already exists.")
     
@@ -53,3 +54,32 @@ def crud_delete_user(user_id:int, db:Session):
 def crud_get_all_users(db:Session):
     all_users = db.query(User).all()
     return all_users
+
+def crud_login(
+    response: Response, 
+    form_data: OAuth2PasswordRequestForm = Depends(), 
+    db: Session = Depends(get_db)
+):
+    user = db.query(User).filter(User.username == form_data.username).first()
+    if not user or not verify_password(form_data.password, user.password):
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    session_token = create_session(db, user.id)
+    response.set_cookie(
+        key=COOKIE_NAME,
+        value=session_token,
+        httponly=True,
+        secure=False,
+        samesite="lax",
+        max_age=7 * 24 * 60 * 60
+    )
+    return {"message": f"Welcome to Historia Chat, {user.name}!"}
+
+def crud_logout(response: Response, request: Request, db: Session = Depends(get_db)):
+    session_token = request.cookies.get(COOKIE_NAME)
+    
+    if session_token:
+        db.query(UserSession).filter(UserSession.session_token == session_token).delete()
+        db.commit()
+    
+    response.delete_cookie(COOKIE_NAME)
+    return {"message": "Successfully logged out."}
