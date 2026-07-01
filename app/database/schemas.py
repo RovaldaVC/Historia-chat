3d2cbfb10c3f9c6bfad025280e866656954633e7
rovaldavc <arshia.talimian86@gmail.com>
2026-07-01 19:07:54 +0330

    No max length check on password before hashing IS NOW FIXED

--- app/database/schemas.py
+++ app/database/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 
@@ -6,7 +6,11 @@ class UserCreate(BaseModel):
     name : str
     family: Optional[str] = None
     username: str
-    password: str
+    password: str = Field(
+        min_length=8,
+        max_length=128,
+        description= "Password must be between 8 and 128 characters"
+    )
     role: str  = "user"
     active: bool
     
